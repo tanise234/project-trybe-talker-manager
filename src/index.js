7262a95866ed3469b6da897b0f1311884f43adaa
Tanise Santos <tanise234@gmail.com>
2022-08-26 17:39:14 -0300

    Req 3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routerTalker = require('../routes/routesTalker');
+const routerLogin = require('../routes/routesLogin');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(routerTalker);
+app.use(routerLogin);
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
